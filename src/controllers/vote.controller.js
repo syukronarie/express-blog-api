6bc6b386215a618fddc87a1621e07bfe468bc435
syukronarie <syukronarie@gmail.com>
2022-08-21 21:40:37 +0700

    Initial vote api - Added vote controller - Added vote model - Added vote route - Added vote validation

--- src/controllers/vote.controller.js
+++ src/controllers/vote.controller.js
@@ -1,10 +1,10 @@
 const catchAsync = require("../utils/catchAsync");
 
-const createVote = catchAsync(async (req, res) => {});
-const getVotes = catchAsync(async (req, res) => {});
-const getVote = catchAsync(async (req, res) => {});
-const updateVote = catchAsync(async (req, res) => {});
-const deleteVote = catchAsync(async (req, res) => {});
+const createVote = catchAsync(async (_req, _res) => {});
+const getVotes = catchAsync(async (_req, _res) => {});
+const getVote = catchAsync(async (_req, _res) => {});
+const updateVote = catchAsync(async (_req, _res) => {});
+const deleteVote = catchAsync(async (_req, _res) => {});
 
 module.exports = {
   createVote,

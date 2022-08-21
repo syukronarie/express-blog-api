cb89ba64b610a1d7cfb69ed99a8b36de156a00fc
syukronarie <syukronarie@gmail.com>
2022-08-22 00:29:32 +0700

    Updated ErrorMessages

--- src/utils/ErrorMessages.js
+++ src/utils/ErrorMessages.js
@@ -8,6 +8,7 @@ const ErrorMessage = {
   INTERNAL_SERVER_ERROR: "Internal server error",
   FILE_SIZE_ERR: "Maximum File Size exceeded",
   AUTH_ERROR: "Token is missing, Authentication Failed",
+  REFRESH_AUTH_ERROR: "Refresh Token is missing or expires, Authentication Failed",
   FILE_VALIDATION_ERR: "Only PDF/Jpeg/Png file with max size 5MB is allowed",
   MANDATORY_FIELD_MISSING_ERROR: "Mandatory Field/s missing",
   INVALID_UPLOAD_FORM: "Invalid Details for Form upload",

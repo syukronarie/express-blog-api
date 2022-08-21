0a590284bbc45f81157f7b1e5bcf2dad35aba752
syukronarie <syukronarie@gmail.com>
2022-08-21 21:15:59 +0700

    Updated auth route

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const userRoute = require("./user.route");
 const postRoute = require("./post.route");
-const authRoute = require("./auth.router");
+const authRoute = require("./auth.route");
 
 const router = express.Router();
 

2f07cc48c3909f2c5493a5a3f44021672a85891d
syukronarie <syukronarie@gmail.com>
2022-08-25 10:26:55 +0700

    feat/category: added category model

---
+++ src/models/category.model.js
@@ -0,0 +1,13 @@
+class Category {
+  constructor() {
+    this.title = "";
+    this.description = "";
+    this.slug = "";
+    this.isPublished = "";
+    this.createdAt = "";
+    this.updatedAt = "";
+    this.publishedAt = "";
+  }
+}
+
+module.exports = Category;
